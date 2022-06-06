1b4bfc76bdc25ff15b00a0108567fc02dd30809b
cweijan <cweijan@163.com>
2022-06-06 11:16:50 +0800

    Kill jdbc server after reload vscode.

--- vscode-dbclient-jdbc/extension.js
+++ vscode-dbclient-jdbc/extension.js
@@ -1,14 +1,17 @@
-const vscode = require('vscode');
+const {platform} = require("os");
+const { exec } = require("child_process")
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {}
-
-// this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
+function activate(context) {
+    const filterCmd = platform() === 'win32' ? 'findstr' : 'grep';
+    exec(`jps|${filterCmd} jdbc-adapter-server`, (err, stdout) => {
+        const pid = stdout?.match(/\d+/)?.[0]
+        if (pid) {
+            process.kill(pid)
+        }
+    })
 }
+
+function deactivate() {
+}
+
+module.exports = {activate, deactivate}
